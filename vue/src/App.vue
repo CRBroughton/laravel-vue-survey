cf98b633213c28aade3fe8991246e69df5801e73
CRBroughton <crbroughton@posteo.uk>
2022-10-06 16:10:57 +0100

    feat(front-end): add tailwind form

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
+  import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+  <HelloWorld />
 </template>
 
 <style scoped>
