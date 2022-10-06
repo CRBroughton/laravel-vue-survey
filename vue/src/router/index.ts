911cfd2247bc387dbc0f6250c9721050c08b9b0b
CRBroughton <crbroughton@posteo.uk>
2022-10-06 16:27:30 +0100

    feat(front-end): add defaultLayout

--- vue/src/router/index.ts
+++ vue/src/router/index.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory, RouteRecordRaw} from 'vue-router'
+import DefaultLayout from '../components/DefaultLayout.vue'
 import Dashboard from '../views/Dashboard.vue'
 import Login from '../views/Login.vue'
 import Register from '../views/Register.vue'
@@ -6,8 +7,12 @@ import Register from '../views/Register.vue'
 const routes: RouteRecordRaw[] = [
     {
         path: '/',
+        redirect: '/dashboard',
         name: 'Dashboard',
-        component: Dashboard
+        component: DefaultLayout,
+        children: [
+            { path: 'dashboard', name: 'Dashboard', component: Dashboard }
+        ]
     },
     {
         path: '/login',
