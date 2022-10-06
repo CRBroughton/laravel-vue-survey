6dd3f66c3195eadad84e8b793e8b8c5b52db222c
CRBroughton <crbroughton@posteo.uk>
2022-10-06 16:55:01 +0100

    feat(front-end): add router links for views

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -6,7 +6,7 @@
         <p class="mt-2 text-center text-sm text-gray-600">
             Or
             {{ ' ' }}
-            <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">start your 14-day free trial</a>
+            <router-link :to="{ name: 'Register' }" class="font-medium text-indigo-600 hover:text-indigo-500">register for free</router-link>
         </p>
     </div>
     <form class="mt-8 space-y-6" action="#" method="POST">
@@ -23,18 +23,6 @@
                 <input id="password" name="password" type="password" autocomplete="current-password" required=""
                     class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                     placeholder="Password" />
-            </div>
-        </div>
-
-        <div class="flex items-center justify-between">
-            <div class="flex items-center">
-                <input id="remember-me" name="remember-me" type="checkbox"
-                    class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500" />
-                <label for="remember-me" class="ml-2 block text-sm text-gray-900">Remember me</label>
-            </div>
-
-            <div class="text-sm">
-                <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Forgot your password?</a>
             </div>
         </div>
 
