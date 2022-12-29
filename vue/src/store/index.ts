6d1073e730247408ed0ec1d72210c632dbd4dff9
CRBroughton <crbroughton@posteo.uk>
2022-12-29 13:44:41 +0000

    feat(front-end): :sparkles: users can now register

--- vue/src/store/index.ts
+++ vue/src/store/index.ts
@@ -9,7 +9,7 @@ interface State {
       email?: string
       imageUrl?: string
     }
-    token: number | null
+    token: string | null
   }
 }
 
@@ -18,15 +18,36 @@ export const store = createStore<State>({
     user: {
       data: {
       },
-      token: null,
+      token: sessionStorage.getItem('TOKEN'),
     },
   },
   getters: {},
-  actions: {},
+  actions: {
+    register({ commit }, user) {
+      return fetch('http://localhost:8000/api/register', {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+        },
+        method: 'POST',
+        body: JSON.stringify(user),
+      })
+        .then(res => res.json())
+        .then((res) => {
+          commit('setUser', res)
+          return res
+        })
+    },
+  },
   mutations: {
     logout: (state) => {
       state.user.data = {}
       state.user.token = null
+    },
+    setUser: (state, userData) => {
+      state.user.token = userData.token
+      state.user.data = userData.user
+      sessionStorage.setItem('TOKEN', userData.token)
     },
   },
   modules: {},
