019a8c5367e75b533e5a0a55a772b086f7ca7031
CRBroughton <crbroughton@posteo.uk>
2023-01-07 12:26:46 +0000

    feat(front-end): :sparkles: mock surveys now render to the browser

--- vue/src/views/Surveys.vue
+++ vue/src/views/Surveys.vue
@@ -22,8 +22,14 @@ const surveys = computed(() => {
         </router-link>
       </div>
     </template>
-    <div>
-      <pre>{{ surveys }}</pre>
+    <div class="grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3">
+      <div v-for="survey in surveys" :key="survey.id" class="flex flex-col py-4 px-6 shadow-md bg-white hover:bg-gray-50 h-[470px]">
+        <img :src="survey.image" alt="survey image" class="w-full h-48 object-cover">
+        <h4 class="mt-4 text-lg font-bold">
+          {{ survey.title }}
+        </h4>
+        <div class="overflow-hidden flex-1" v-html="survey.description" />
+      </div>
     </div>
   </PageComponent>
 </template>
